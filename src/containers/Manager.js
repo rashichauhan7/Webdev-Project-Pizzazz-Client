e66bdcbc2b7c76431d2da3835769b0f86ed42d8b
mahale816 <mahale.c@husky.neu.edu>
2018-08-13 16:37:28 -0400

    login intigration

--- src/containers/Manager.js
+++ src/containers/Manager.js
@@ -5,16 +5,21 @@ import SalonEditor from "../components/SalonEditor";
 import InitialList from "../components/IntialList";
 import Category from "../components/Category"
 import SearchEditor from "../components/SearchEditor"
+import Register from '../components/Register'
+import App from '../components/Login'
+
 export default class Manager extends React.Component {
 
     render() {
         return(
             <Router>
                 <div>
-                <Route path="*" component={Home}></Route>
-                <Route path="/home" component={InitialList}/>
-                <Route path="/category/:category" component = {Category}/>
-                <Route path="/salon/:salonId" component={SalonEditor}></Route>
+                    <Route path='/login' component={App}/>
+                    <Route path='/register' component={Register}/>
+                    <Route path="*" component={Home}></Route>
+                    <Route path="/home" component={InitialList}/>
+                    <Route path="/category/:category" component = {Category}/>
+                    <Route path="/salon/:salonId" component={SalonEditor}></Route>
                     <Route path="/search/:keyword" component={SearchEditor}></Route>
                 </div>
             </Router>
