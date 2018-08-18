006e26f2f61c25fa6cbf36edefd48a8ab6cce61d
rashichauhan7 <rashi.chauhan95@gmail.com>
2018-08-18 19:16:38 -0400

    texting facebook

--- src/components/Login.js
+++ src/components/Login.js
@@ -71,21 +71,22 @@ class App extends Component {
 
 
     facebookResponse = (response) => {
-        const tokenBlob = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)], {type : 'application/json'});
-        const options = {
-            method: 'POST',
-            body: tokenBlob,
-            mode: 'cors',
-            cache: 'default'
-        };
-        fetch('http://localhost:4000/api/v1/auth/facebook', options).then(r => {
-            const token = r.headers.get('x-auth-token');
-            r.json().then(user => {
-                if (token) {
-                    this.setState({isAuthenticated: true, user, token})
-                }
-            });
-        })
+        console.log(response);
+        // const tokenBlob = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)], {type : 'application/json'});
+        // const options = {
+        //     method: 'POST',
+        //     body: tokenBlob,
+        //     mode: 'cors',
+        //     cache: 'default'
+        // };
+        // fetch('http://localhost:4000/api/v1/auth/facebook', options).then(r => {
+        //     const token = r.headers.get('x-auth-token');
+        //     r.json().then(user => {
+        //         if (token) {
+        //             this.setState({isAuthenticated: true, user, token})
+        //         }
+        //     });
+        // })
     };
 
     googleResponse = (response) => {
